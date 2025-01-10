030686723cd11d9d97193cb74ed7708cf2e9ace9
Thiago Leite <leite.tjs@gmail.com>
2025-01-10 18:01:48 -0300

    refactor: change messages file local

---
+++ src/utils/messages.ts
@@ -0,0 +1,6 @@
+export default {
+  employeeNotfound: 'Funcionário não encontrado',
+  registerOpenNotFound: 'Registro aberto não encontrado',
+  openRegisterError: 'Funcionário já está com um ponto aberto',
+  registerEntryTime: 'Registro realizado com sucesso',
+};
