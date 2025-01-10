d45ceb15009f336d6acb0fcd58fab005b7882769
Thiago Leite <leite.tjs@gmail.com>
2025-01-10 19:25:00 -0300

    feat: generate employee code controller

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
-import { ResourceNotFoundError } from '@errors/resource-not-found';
-import { employeeRoute } from '@routes/employee-route';
-import { healthRoute } from '@routes/health-route';
-import { env } from 'env';
 import fastify from 'fastify';
 import { ZodError } from 'zod';
+import { healthRoute } from '../routes/health-route';
+import { employeeRoute } from '../routes/employee-route';
+import { ResourceNotFoundError } from '../errors/resource-not-found';
+import { env } from '../env';
 
 export const app = fastify();
 
@@ -29,6 +29,8 @@ app.setErrorHandler((error, _, reply) => {
   if (error instanceof ResourceNotFoundError) {
     return reply.status(401).send({ message: error.message });
   }
+
+  console.log(error);
 
   return reply.status(500).send({ message: 'Internal Server Error' });
 });
--- src/routes/employee-route.ts
+++ src/routes/employee-route.ts
@@ -1,6 +1,8 @@
-import { employeeLogController } from '@controllers/employee-log-controller';
 import { FastifyInstance } from 'fastify';
+import { employeeLogController } from '../controllers/employee-log-controller';
+import { generateEmployeeCodeController } from '../controllers/generate-employee-code-controller';
 
 export const employeeRoute = async (app: FastifyInstance) => {
   app.post('/', employeeLogController);
+  app.post('/generate-code', generateEmployeeCodeController);
 };
