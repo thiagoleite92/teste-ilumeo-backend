2456e1adcbe78c8c33a9960ecf92aa34ba5120db
Thiago Leite <leite.tjs@gmail.com>
2025-01-10 16:42:40 -0300

    test: exit time service

---
+++ src/services/tests/work-shift-entry-time-service.spec.ts
@@ -0,0 +1,52 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { EmployeeInterface } from '../../repositories/interfaces/employee-interface';
+import { ResourceNotFoundError } from '../../errors/resource-not-found';
+import { InMemoryEmployeeRepository } from '../../repositories/in-memory/in-memory-employee-repository';
+import { InMemoryWorkShiftRepository } from '../../repositories/in-memory/in-memory-work-shfit-repository';
+import { WorkShiftInterface } from '../../repositories/interfaces/workshift-interface';
+import { WorkShiftEntryTimeService } from '../work-shift-entry-time-service';
+import { OpenRegisterError } from '../../errors/open-register';
+
+let employeeRepository: EmployeeInterface;
+let workShiftRepository: WorkShiftInterface;
+let sut: WorkShiftEntryTimeService;
+describe('Service => work shit entry', () => {
+  beforeEach(() => {
+    employeeRepository = new InMemoryEmployeeRepository();
+    workShiftRepository = new InMemoryWorkShiftRepository();
+    sut = new WorkShiftEntryTimeService(
+      workShiftRepository,
+      employeeRepository
+    );
+  });
+
+  it('should throw an error => ResourceNotFound with not existing employeeCode', async () => {
+    await expect(() => sut.execute('randomCode')).rejects.toBeInstanceOf(
+      ResourceNotFoundError
+    );
+  });
+
+  it('should be able to register an entry time', async () => {
+    const employeeCode = await employeeRepository.generateEmployeeCode();
+
+    await sut.execute(employeeCode);
+
+    const history = await employeeRepository.findByCode(employeeCode);
+
+    expect([history]).toHaveLength(1);
+  });
+
+  it('shoulde throw an error => OpenRegisterError with a register already open', async () => {
+    const employeeCode = await employeeRepository.generateEmployeeCode();
+
+    const employee = await employeeRepository.findByCode(employeeCode);
+
+    await workShiftRepository.registerEmployeeEntryTime({
+      employeeId: employee!.id,
+    });
+
+    await expect(() => sut.execute(employeeCode)).rejects.toBeInstanceOf(
+      OpenRegisterError
+    );
+  });
+});
---
+++ src/services/tests/work-shift-exit-time-service.spec.ts
@@ -0,0 +1,67 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { EmployeeInterface } from '../../repositories/interfaces/employee-interface';
+import { ResourceNotFoundError } from '../../errors/resource-not-found';
+import { InMemoryEmployeeRepository } from '../../repositories/in-memory/in-memory-employee-repository';
+import { InMemoryWorkShiftRepository } from '../../repositories/in-memory/in-memory-work-shfit-repository';
+import { WorkShiftInterface } from '../../repositories/interfaces/workshift-interface';
+import { WorkShiftExitTimeService } from '../work-shift-exit-time-service';
+
+let employeeRepository: EmployeeInterface;
+let workShiftRepository: WorkShiftInterface;
+let sut: WorkShiftExitTimeService;
+describe('Service => work shit exit', () => {
+  beforeEach(() => {
+    employeeRepository = new InMemoryEmployeeRepository();
+    workShiftRepository = new InMemoryWorkShiftRepository();
+    sut = new WorkShiftExitTimeService(workShiftRepository, employeeRepository);
+  });
+
+  it('should throw an error => ResourceNotFound with not existing employeeCode', async () => {
+    await expect(() => sut.execute('randomCode')).rejects.toBeInstanceOf(
+      ResourceNotFoundError
+    );
+  });
+
+  it('should throw an error => ResourceNotFound with no open register', async () => {
+    const employeeCode = await employeeRepository.generateEmployeeCode();
+
+    const employee = await employeeRepository.findByCode(employeeCode);
+
+    await workShiftRepository.registerEmployeeEntryTime({
+      employeeId: employee!.id,
+    });
+
+    const openRegister = await workShiftRepository.findOpenRegister({
+      employeeId: employee!.id,
+    });
+
+    await workShiftRepository.registerEmployeeExitTime({
+      employeeId: employee!.id,
+      workShiftId: openRegister!.id,
+    });
+
+    await expect(() => sut.execute(employeeCode)).rejects.toBeInstanceOf(
+      ResourceNotFoundError
+    );
+  });
+
+  it('should be able to register exit', async () => {
+    const employeeCode = await employeeRepository.generateEmployeeCode();
+
+    const employee = await employeeRepository.findByCode(employeeCode);
+
+    await workShiftRepository.registerEmployeeEntryTime({
+      employeeId: employee!.id,
+    });
+
+    const register = await workShiftRepository.findOpenRegister({
+      employeeId: employee!.id,
+    });
+
+    expect(register?.exitTime).toBeNull();
+
+    await sut.execute(employeeCode);
+
+    expect(register?.exitTime).not.toBeNull();
+  });
+});
